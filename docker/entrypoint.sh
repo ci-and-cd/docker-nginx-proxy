04f45e0c016861ef9500e5783ec2c6181226c4a4
haolun zhang <opensource@infra.top>
2018-09-27 23:45:05 +0800

    fix enrtypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,35 +5,27 @@ set -e
 echo "USER ${USER}"
 echo "ls -la /etc/letsencrypt"
 ls -la /etc/letsencrypt
-if [ ! -L /etc/letsencrypt/live ]; then
-    # /etc/letsencrypt/archive/<domain>/*
-    # /etc/letsencrypt/keys/<domain>/cert.pem -> ../../archive/<domain>/cert1.pem
-    # /etc/letsencrypt/keys/<domain>/chain.pem -> ../../archive/<domain>/chain1.pem
-    # /etc/letsencrypt/keys/<domain>/fullchain.pem -> ../../archive/<domain>/fullchain1.pem
-    # /etc/letsencrypt/keys/<domain>/privkey.pem -> ../../archive/<domain>/privkey1.pem
-
-    if [ "${USER}" != "root" ]; then
-        sudo ln -s /etc/letsencrypt/keys /etc/letsencrypt/live
-        sudo chown nginx:nginx /etc/letsencrypt/live
-    else
-        ln -s /etc/letsencrypt/keys /etc/letsencrypt/live
-        chown nginx:nginx /etc/letsencrypt/live
-    fi
-fi
-
-
-if [ "${USER}" != "root" ]; then
-    sudo mkdir -p /etc/nginx/certs/live
-    sudo cp -Lrf /etc/letsencrypt/live/. /etc/nginx/certs/live/
-    sudo chown -R nginx:nginx /etc/nginx/certs/live
-else
-    mkdir -p /etc/nginx/certs/live/keys
-    cp -Lrf /etc/letsencrypt/live/. /etc/nginx/certs/live/
-    chown -R nginx:nginx /etc/nginx/certs/live
-fi
-
 
 if ([ "$1" == 'sudo' ] && [ "$2" == nginx ]) || [ "$1" == 'nginx' ]; then
+
+    # /etc/nginx/certs/live -> /etc/letsencrypt/keys
+    if [ ! -L /etc/letsencrypt/live ]; then
+        # /etc/letsencrypt/archive/<domain>/*
+        # /etc/letsencrypt/keys/<domain>/cert.pem -> ../../archive/<domain>/cert1.pem
+        # /etc/letsencrypt/keys/<domain>/chain.pem -> ../../archive/<domain>/chain1.pem
+        # /etc/letsencrypt/keys/<domain>/fullchain.pem -> ../../archive/<domain>/fullchain1.pem
+        # /etc/letsencrypt/keys/<domain>/privkey.pem -> ../../archive/<domain>/privkey1.pem
+
+        if [ "${USER}" != "root" ]; then
+            sudo ln -s /etc/letsencrypt/keys /etc/letsencrypt/live
+            sudo chown -R nginx:nginx /etc/letsencrypt/live
+        else
+            ln -s /etc/letsencrypt/keys /etc/letsencrypt/live
+            chown nginx:nginx -R /etc/letsencrypt/live
+        fi
+    fi
+    #cp -Lrf /etc/letsencrypt/live/. /etc/nginx/certs/live/
+
     /render.sh "/etc/nginx/conf.d"
 
     if [ -n "${NONSECUREPORT}" ]; then
