499339f9abdf1cfaf544b843860af1324e22847e
haolun zhang <opensource@infra.top>
2018-09-27 20:53:47 +0800

    update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,11 +2,17 @@
 
 set -e
 
-echo "whoami $(whoami)"
+echo "USER ${USER}"
 echo "ls -la /etc/letsencrypt"
 ls -la /etc/letsencrypt
 if [ ! -L /etc/letsencrypt/live ]; then
-    if [ "$(whoami)" != "root" ]; then
+    # /etc/letsencrypt/archive/<domain>/*
+    # /etc/letsencrypt/keys/<domain>/cert.pem -> ../../archive/<domain>/cert1.pem
+    # /etc/letsencrypt/keys/<domain>/chain.pem -> ../../archive/<domain>/chain1.pem
+    # /etc/letsencrypt/keys/<domain>/fullchain.pem -> ../../archive/<domain>/fullchain1.pem
+    # /etc/letsencrypt/keys/<domain>/privkey.pem -> ../../archive/<domain>/privkey1.pem
+
+    if [ "${USER}" != "root" ]; then
         sudo ln -s /etc/letsencrypt/keys /etc/letsencrypt/live
         sudo chown nginx:nginx /etc/letsencrypt/live
     else
@@ -14,6 +20,18 @@ if [ ! -L /etc/letsencrypt/live ]; then
         chown nginx:nginx /etc/letsencrypt/live
     fi
 fi
+
+
+if [ "${USER}" != "root" ]; then
+    sudo mkdir -p /etc/nginx/certs/live
+    sudo cp -Lrf /etc/letsencrypt/live/. /etc/nginx/certs/live/
+    sudo chown -R nginx:nginx /etc/nginx/certs/live
+else
+    mkdir -p /etc/nginx/certs/live/keys
+    cp -Lrf /etc/letsencrypt/live/. /etc/nginx/certs/live/
+    chown -R nginx:nginx /etc/nginx/certs/live
+fi
+
 
 if ([ "$1" == 'sudo' ] && [ "$2" == nginx ]) || [ "$1" == 'nginx' ]; then
     /render.sh "/etc/nginx/conf.d"
